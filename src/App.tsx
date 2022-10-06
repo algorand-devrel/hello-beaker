de2d986a4fb53ec3bbc48e0aff3b0f0e822eeef6
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-06 08:56:09 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import algosdk from 'algosdk'
+import { HelloBeaker } from './hellobeaker_client'
+import * as bkr from 'beaker-ts'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [acct, setAccount] = useState<algosdk.Account|undefined>(undefined)
+  const [appClient, setAppClient] = useState<HelloBeaker|undefined>(undefined)
+  const [appId, setAppId] = useState<number|undefined>(undefined);
+
+  useEffect(()=>{
+    if(acct !== undefined) return
+    bkr.sandbox.getAccounts().then((accts)=>{
+      const acct = accts[0]
+      setAccount({addr: acct.addr, sk: acct.privateKey} as algosdk.Account)
+
+      setAppClient(new HelloBeaker({
+        client: bkr.sandbox.getAlgodClient(),
+        sender: acct.addr,
+        signer: acct.signer,
+      }))
+    });
+  })
+
+
+  async function createApp() {
+    if(appClient === undefined || acct === undefined) return;
+
+    const [appId, , ] = await appClient?.create()
+
+    setAppId(appId)
+    setAppClient(new HelloBeaker({
+      client: appClient.client,
+      sender: appClient.sender,
+      signer: appClient.signer,
+      appId: appId,
+    }))
+  }
+
+  async function greet(){
+    if(appClient === undefined) return;
+
+    const result = await appClient.hello({name:"ben"})
+    alert(result.value)
+  }
+
+  const action = appId === undefined ?  <button onClick={createApp}>Create</button>:<button onClick={greet}>Greet</button>
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+        <h3>{acct?.addr}</h3>
+        {action}
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
