b3ee98e5f55d7935b4a4826fe114d80a321ca908
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-06 09:46:40 -0400

    stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+Hello Beaker Example
+--------------------
+
+
+```sh
+git clone git@github.com:algorand-devrel/hello-beaker.git
+cd hello-beaker
+yarn #installs stuff
+yarn start dev # starts the dev server
+```
--- src/App.tsx
+++ src/App.tsx
@@ -51,17 +51,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-
         <h3>{acct?.addr}</h3>
         {action}
         <p>
