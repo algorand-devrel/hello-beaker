2b47b873fd0d6bbaf30d2c278e791d410b653543
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-06 10:00:51 -0400

    adding name input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import algosdk from 'algosdk'
 import { HelloBeaker } from './hellobeaker_client'
 import * as bkr from 'beaker-ts'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [acct, setAccount] = useState<algosdk.Account|undefined>(undefined)
   const [appClient, setAppClient] = useState<HelloBeaker|undefined>(undefined)
   const [appId, setAppId] = useState<number|undefined>(undefined);
@@ -31,6 +29,8 @@ function App() {
 
     const [appId, , ] = await appClient?.create()
 
+    console.log(`Created app: ${appId}`)
+
     setAppId(appId)
     setAppClient(new HelloBeaker({
       client: appClient.client,
@@ -42,12 +42,18 @@ function App() {
 
   async function greet(){
     if(appClient === undefined) return;
-
-    const result = await appClient.hello({name:"ben"})
+    const ta : HTMLTextAreaElement | null = document.getElementById('name') as HTMLTextAreaElement
+    const result = await appClient.hello({name:ta.value})
     alert(result.value)
   }
 
-  const action = appId === undefined ?  <button onClick={createApp}>Create</button>:<button onClick={greet}>Greet</button>
+  const action = appId === undefined ?  <button onClick={createApp}>Create</button>:(
+    <div>
+      <textarea id='name' placeholder='what is your name?'></textarea>
+      <hr />
+      <button onClick={greet}>Greet</button>
+    </div>
+  )
 
   return (
     <div className="App">
